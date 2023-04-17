28d26599ed168be6f2e615c21a6e2588fbcec82a
Harold888 <99030057+Harold888@users.noreply.github.com>
2023-04-17 15:50:17 -0500

    v 0.0.2

---
+++ ClienteAPI.js
@@ -0,0 +1,15 @@
+
+const url="https://jsonplaceholder.typicode.com/todos/";
+
+async function obtenerTodo(id){
+
+    //Versiones > 18 de node.js
+    const respuesta = await fetch(`${url}${id}`);
+    const datos = await respuesta.json();
+    procesarJson(datos);
+
+}
+
+function procesarJson(dato){
+    console.log(dato);
+}
